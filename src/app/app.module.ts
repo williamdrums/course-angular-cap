493f410487737cbb54a8f6cb3c49862fea9c589e
William Nascimento <william.nascimento@capgemini.com>
2021-09-06 09:41:40 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { EventsListComponent } from './events-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './event-thumbnail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
